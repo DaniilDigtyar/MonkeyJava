b90398a9e9894870158193c829a2e460f1a39267
Joan Domingo <joandg1997@hotmail.com>
2018-04-26 18:32:14 +0200

    gcode als treballadors  i algo mes

---
+++ AM3D/AM3D/src/Treballdor.java
@@ -0,0 +1,55 @@
+
+public class Treballdor {
+	private String dni;
+	private String nom;
+	private String cognoms;
+	private String gcode;
+
+	
+	public Treballdor(String dni, String nom, String cognoms,String gcode) {
+		super();
+		this.dni = dni;
+		this.nom = nom;
+		this.cognoms = cognoms;
+		this.gcode = gcode;
+
+	}
+
+	public String getDni() {return dni;}
+	public void setDni(String dni) {this.dni = dni;}
+
+	public String getNom() {return nom;}
+	public void setNom(String nom) {this.nom = nom;}
+
+	public String getCognoms() {return cognoms;}
+	public void setCognoms(String cognoms) {this.cognoms = cognoms;}
+	
+	public String getGcode() {return gcode;}
+	public void setGcode(String gcode) {this.gcode = gcode;}
+
+
+	@Override
+	public String toString() {
+		
+		String espai1 = "";
+		if (getNom().length()<=14){
+			int length = 16-getNom().length();
+			for(int i=0; i<length;i++){
+				espai1 = espai1 + " ";
+			}
+		}
+		String espai2 = "";
+		if (getCognoms().length()<=20){
+			int length = 22-getCognoms().length();
+			for(int i=0; i<length;i++){
+				espai2 = espai2 + " ";
+			}
+		}
+		
+		return dni + "  " +  nom + espai1 + cognoms + espai2 + gcode;
+	}
+	
+	
+	
+
+}
